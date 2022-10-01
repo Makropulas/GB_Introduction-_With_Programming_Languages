f19eb7cf238054c368f8697d168ecc04772baaf6
DmitryMalomoshin <makropulas@gmail.com>
2022-10-02 01:18:29 +0300

    Add Task_8

---
+++ Seminar1_30.09/Task_8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Введите целое число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        int i = 2;
+
+        Console.WriteLine();
+
+        if (number < 2) Console.WriteLine("Введённое число не соответствует условиям задачи");
+        else
+        {
+            while (i < number - 1)
+            {
+                Console.Write($"{i}, ");
+                i = i + 2;
+            }
+            Console.Write($"{i}");
+        }
+
+    }
+}
