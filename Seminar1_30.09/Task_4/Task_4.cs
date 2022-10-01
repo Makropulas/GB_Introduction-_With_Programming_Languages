fbdaeaf88653dabd7c826e05e2ac55418a624a9b
DmitryMalomoshin <makropulas@gmail.com>
2022-10-01 17:01:57 +0300

    Add Task_4

---
+++ Seminar1_30.09/Task_4/Task_4.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("ВВЕДИТЕ ТРИ ЦЕЛЫХ ЧИСЛА И УЗНАЙТЕ, КАКОЕ ИЗ НИХ ЯВЛЯЕТСЯ МАКСИМАЛЬНЫМ \n");
+
+        Console.Write("Введите первое число: ");
+        int number1 = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите второе число: ");
+        int number2 = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите третье число: ");
+        int number3 = int.Parse(Console.ReadLine()!);
+
+        int max = number1;
+
+        Console.WriteLine();
+
+        if (number2 > number1) max = number2;
+        if (number3 > number2) max = number3;
+        Console.WriteLine($"Максимальное из трёх чисел: {max}");
+    }
+}
