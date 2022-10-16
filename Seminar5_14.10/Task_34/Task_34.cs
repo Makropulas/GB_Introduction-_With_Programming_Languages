ae152285540a6e94f08ca47194c1da4b432e80fc
DmitryMalomoshin <makropulas@gmail.com>
2022-10-16 22:34:23 +0300

    Add Task_34

---
+++ Seminar5_14.10/Task_34/Task_34.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+namespace DZ_Seminar5
+{
+    internal class Task_34
+    {
+        private static void Main(string[] args)
+        {
+            int[] array = new int[new Random().Next(4, 10)];
+            FillArray(array);
+            Console.WriteLine($"[{String.Join(", ", array)}]");
+
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0) count++;
+            }
+            Console.WriteLine($"Количество чётных чисел в массиве: {count}");
+        }
+
+        public static void FillArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(100, 1000);
+            }
+        }
+    }
+}
