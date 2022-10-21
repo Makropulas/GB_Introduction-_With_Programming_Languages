305e0a0f2a4a1f905ab7f31becc4ac7d9fbc01af
DmitryMalomoshin <makropulas@gmail.com>
2022-10-21 23:49:33 +0300

    Add Task_50

---
+++ Seminar7_21.10/Task_50/Task_50.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// i = 4, j = 2 -> такого числа в массиве нет
+// i = 1, j = 2 -> 2
+
+
+namespace DZ_Seminar7
+{
+    internal class Task_50
+    {
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int[,] array = GetArray(4, 5, 0, 9);
+
+            Console.WriteLine("Задан массив:\n");
+            PrintArray(array);
+            Console.WriteLine();
+
+            Console.Write("Введите индекс строки:  ");
+            int i = int.Parse(Console.ReadLine()!);
+            Console.Write("Введите индекс столбца: ");
+            int j = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine();
+            PrintElementValue(array, i, j);
+        }
+        public static int[,] GetArray(int m, int n, int minValue, int maxValue)
+        {
+            int[,] result = new int[m, n];
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    result[i, j] = new Random().Next(minValue, maxValue + 1);
+                }
+            }
+            return result;
+        }
+        public static void PrintArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+       public static void PrintElementValue (int[,] arr, int i, int j)
+        {
+            if(i >= arr.GetLength(0) || j >= arr.GetLength(1)) Console.WriteLine("Такого элемента в массиве нет!");
+            else Console.WriteLine($"Значение запрошенного элемента: {arr[i, j]}");
+        }
+    }
+}
